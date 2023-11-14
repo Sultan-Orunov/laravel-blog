65b62a9dd9f9e393429a68f88afefffe090be16f
Sultan-Orunov <osbfswd@gmail.com>
2023-11-14 17:35:22 +0500

    Добавил загрузку изображений для статьи

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -27,22 +27,57 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.posts.store') }}" method="post">
+                        <form action="{{ route('admin.posts.store') }}" method="post" enctype="multipart/form-data">
                             @csrf
-                            <div class="form-group w-25">
-                                <input type="text" value="{{ old('title') }}" class="form-control" name="title" placeholder="Название статьи...">
+                            <div class="form-group w-50">
+                                <input type="text" value="{{ old('title') }}" class="form-control" name="title"
+                                       placeholder="Название статьи...">
                                 @error('title')
+                                <div class="text-danger">
+                                    {{$message}}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input name="preview_image" value="{{ old('preview_image') }}" type="file"
+                                               class="custom-file-input" id="previewImage">
+                                        <label class="custom-file-label" for="previewImage">Выберите превью изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                                @error('preview_image')
                                     <div class="text-danger">
-                                        {{$message}}
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input name="main_image" value="{{ old('main_image') }}" type="file"
+                                               class="custom-file-input" id="mainImage">
+                                        <label class="custom-file-label" for="mainImage">Выберите главное изображение</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Загрузка</span>
+                                    </div>
+                                </div>
+                                @error('main_image')
+                                    <div class="text-danger">
+                                        {{ $message }}
                                     </div>
                                 @enderror
                             </div>
                             <div class="form-group">
                                 <textarea id="summernote" name="content">{{ old('content') }}</textarea>
                                 @error('content')
-                                <div class="text-danger">
-                                    {{$message}}
-                                </div>
+                                    <div class="text-danger">
+                                        {{$message}}
+                                    </div>
                                 @enderror
                             </div>
                             <div class="form-group">
