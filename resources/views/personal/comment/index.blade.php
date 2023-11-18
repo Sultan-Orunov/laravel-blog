2c492518f3264dd55c37505d2134019fe5b2c670
Sultan-Orunov <osbfswd@gmail.com>
2023-11-18 21:29:37 +0500

    отображение коментариев пользователя в личном кибинете

--- resources/views/personal/comment/index.blade.php
+++ resources/views/personal/comment/index.blade.php
@@ -25,7 +25,35 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-
+                    <div class="col-12">
+                        <table class="table table-bordered table-hover ">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Заголовок</th>
+                                <th>Действия</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($comments as $comment)
+                                <tr>
+                                    <td>{{ $comment->id }}</td>
+                                    <td class="w-25">{{ $comment->message }}</td>
+                                    <td>
+                                        <div class="d-flex justify-content-around align-content-center">
+                                            <a href="" class="text-info"><i class="fa fa-eye"></i></a>
+                                            <form action="" method="post">
+                                                @csrf @method('delete')
+                                                <button type="submit" class="border-0 bg-transparent text-danger"><i class="fa fa-trash"></i></button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- ./col -->
                 </div>
                 <!-- /.row -->
             </div><!-- /.container-fluid -->
